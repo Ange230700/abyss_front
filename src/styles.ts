5edc7f6696351e14a8460605cefab3b35c8bef4b
Ange230700 <kouakouangeericstephane@gmail.com>
2025-07-14 22:48:59 +0200

    chore: do stuff

--- src/styles.ts
+++ src/styles.ts
@@ -5,35 +5,61 @@ import Lara from '@primeng/themes/lara';
 
 export const MyPreset = definePreset(Lara, {
   semantic: {
+    primary: {
+      50: '{emerald.50}',
+      100: '{emerald.100}',
+      200: '{emerald.200}',
+      300: '{emerald.300}',
+      400: '{emerald.400}',
+      500: '{emerald.500}',
+      600: '{emerald.600}',
+      700: '{emerald.700}',
+      800: '{emerald.800}',
+      900: '{emerald.900}',
+      950: '{emerald.950}',
+    },
     colorScheme: {
       light: {
-        primary: {
-          50: '{emerald.50}',
-          100: '{emerald.100}',
-          200: '{emerald.200}',
-          300: '{emerald.300}',
-          400: '{emerald.400}',
-          500: '{emerald.500}',
-          600: '{emerald.600}',
-          700: '{emerald.700}',
-          800: '{emerald.800}',
-          900: '{emerald.900}',
-          950: '{emerald.950}',
+        surface: {
+          0: '#ffffff',
+          50: '{neutral.50}',
+          100: '{neutral.100}',
+          200: '{neutral.200}',
+          300: '{neutral.300}',
+          400: '{neutral.400}',
+          500: '{neutral.500}',
+          600: '{neutral.600}',
+          700: '{neutral.700}',
+          800: '{neutral.800}',
+          900: '{neutral.900}',
+          950: '{neutral.950}',
         },
       },
       dark: {
-        primary: {
-          50: '{emerald.50}',
-          100: '{emerald.100}',
-          200: '{emerald.200}',
-          300: '{emerald.300}',
-          400: '{emerald.400}',
-          500: '{emerald.500}',
-          600: '{emerald.600}',
-          700: '{emerald.700}',
-          800: '{emerald.800}',
-          900: '{emerald.900}',
-          950: '{emerald.950}',
+        surface: {
+          0: '#ffffff',
+          50: '{zinc.50}',
+          100: '{zinc.100}',
+          200: '{zinc.200}',
+          300: '{zinc.300}',
+          400: '{zinc.400}',
+          500: '{zinc.500}',
+          600: '{zinc.600}',
+          700: '{zinc.700}',
+          800: '{zinc.800}',
+          900: '{zinc.900}',
+          950: '{zinc.950}',
+        },
+      },
+    },
+  },
+  components: {
+    menubar: {
+      colorScheme: {
+        light: {
+          root: {
+            background: '{surface.200}',
+          },
         },
       },
     },
